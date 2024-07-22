3dcf6e05f21c7c7bd1172acdcfb131cf842677cf
eivanliongson <51158097+eivanliongson@users.noreply.github.com>
2024-07-22 19:24:31 +0800

    add rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://kaleidoscopic-seahorse-4be2c5.netlify.app"
+});
